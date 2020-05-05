221bdadc608534e96940e605514adbeb5d4a21b6
michael-williamson <michaeldavid1721@yahoo.com>
2020-05-05 17:03:25 -0500

    change some code around

--- server/index.js
+++ server/index.js
@@ -5,9 +5,6 @@ app.get('/',(req,res)=>{
     res.send('hi there');
 });
 
-const PORT = process.env.PORT || 5000; 
-app.listen(PORT);
-
 if(process.env.NODE_ENV === 'production'){
     //express will serve up production assets
     app.use(express.static('client/build'));
@@ -19,3 +16,7 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
     });
 }
+
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT);
+
