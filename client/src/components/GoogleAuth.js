9e3b3591bb2d67b5547c1eb3c5fe27c2ca45424b
michael-williamson <michaeldavid1721@yahoo.com>
2020-07-10 14:50:07 -0500

    added .env

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {signIn,signOut} from '../actions/index';
-import {clientId} from '../keys/keys';
+
 
 class GoogleAuth extends Component {
     componentDidMount(){
         window.gapi.load('client:auth2', ()=>{
             window.gapi.client.init({
-                clientId,
+                clientId:process.env.REACT_APP_GOOGLE_API_KEY,
                 scope: 'email'
             }).then(()=>{
                 this.auth =  window.gapi.auth2.getAuthInstance(); 
