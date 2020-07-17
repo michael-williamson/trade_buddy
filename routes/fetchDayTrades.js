137e779d924745a8d1de5031951c62748aa2cc3e
michael-williamson <michaeldavid1721@yahoo.com>
2020-07-17 17:29:20 -0500

    added back routes

---
+++ routes/fetchDayTrades.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('user');
+
+
+
+module.exports = app => {
+    app.post('/api/fetchDayTrades',(req,res)=>{  
+        const {id,date} = req.body; 
+        if(!date){
+        User.findOne({googleId:id}).populate(
+            {
+            path:'trades',
+            match: {typeOfTrade:'Day Trade'}
+            }
+        ).then((item)=>{
+            if(item.trades.length === 0){
+                res.send([{message:"No Day Trades at this time"}])
+            }else{
+            res.send(item.trades)
+            }
+        })
+        .catch(err=>console.log(err))
+    }else{
+            User.findOne({googleId:id}).populate(
+                {
+                path:'trades',
+                match: {typeOfTrade:'Day Trade',startDate:date}
+                }
+            ).then((item)=>{
+                if(item.trades.length === 0){
+                    res.send([{message:"No Day Trades at this date"}])
+                }else{
+                res.send(item.trades)
+                }
+            }) 
+            .catch(err=>console.log(err))           
+        }
+
+    
+    });  
+}
---
+++ routes/fetchTrades.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('user');
+
+module.exports = app => {
+    app.post('/api/fetchTrades',(req,res)=>{  
+        const {id} = req.body; 
+
+        User.findOne({googleId:id})
+        .populate('trades')
+        .then((user)=>{
+            res.send(user.trades);
+        })
+        .catch(err=>console.log(err));
+    
+    });  
+}
